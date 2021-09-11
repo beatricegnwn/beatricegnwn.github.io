c80706b47550ba816798388e4d93931752777dd4
beatricegnwn <83218350+beatricegnwn@users.noreply.github.com>
2021-09-11 16:02:12 +0700

    Add files via upload

---
+++ portfolio/navbar.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <html lang="en">
+    <title>Portfolio</title>
+    <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+<nav class="navbar navbar-expand-md navbar-light bg-light">
+    <a href="index.php" class="navbar-brand">BEA</a>
+    <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+        <div class="navbar-nav">
+            <a href="index.php" class="nav-item nav-link">Home</a>
+            <a href="about.php" class="nav-item nav-link">About</a>
+            <a href="contact.php" class="nav-item nav-link">Contact</a>
+        </div>
+    </div>
+</nav>
+
+
+
+<style>
+
+.navbar {
+    padding: 25px !important;
+    margin-top: -20vh;
+    color: white;
+}
+
+nav {
+   bottom: 0px;
+   height: 25px;
+   margin: 0 !important;
+   padding: 10px !important;
+   background-color: transparent;
+
+}
+
+.navbar a {
+  display: inline;
+  color: white;
+  text-align: center;
+  padding: 30px 20px;
+  text-decoration: none;
+  margin: 0
+}
+</style>
+
+
+<body>
+</body>
+
+
